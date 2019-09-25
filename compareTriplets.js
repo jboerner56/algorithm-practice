9b09e213c64058537e64a24e0c10874dc4e935ee
Jason Boerner <jboerner56@gmail.com>
2019-09-25 11:28:26 -0400

    compare w/ for loop

--- compareTriplets.js
+++ compareTriplets.js
@@ -7,3 +7,21 @@ function compare(a0, a1, a2, b0, b1, b2){
     
     return [alice, bob];
 }
+// using for loop
+// set variables
+let alice = [1,2,3];
+let bob = [3,2,1];
+function compare2(a,b){
+    // 
+    let arr1 = 0;
+    let arr2 = 0;
+    for (i = 0; i < a.length; i++){
+        //if the values are equal dont need to add anything to the count
+        if(a[i] === b[i]){
+            // if a[i] if greater than b[i], add one the count of the one that it greater
+        }else{
+            a[i] > b[i] ? arr1++ : arr2++
+        }
+        return [arr1, arr2];
+    }
+}
